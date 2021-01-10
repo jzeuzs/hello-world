7c107e0de6d098b043367ca1a20646ed670fae8c
1chi <Nyes3704@gmail.com>
2021-01-10 13:53:45 +0000

    added custom css, line numbers and language labels

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
         document.querySelectorAll("pre code").forEach((block) => {
                 hljs.highlightBlock(block);
+                hljs.lineNumbersBlock(block);
+                hljs.initLangBlock(block);
         });
 });
