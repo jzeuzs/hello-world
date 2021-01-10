254c14becc0901e449e4217f1add66040ab24b5d
1chi <Nyes3704@gmail.com>
2021-01-10 14:11:49 +0000

    .

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", (event) => {
         document.querySelectorAll("pre code").forEach((block) => {
                 hljs.highlightBlock(block);
-                hljs.lineNumbersBlock(block, { singleLine: true });
+                hljs.lineNumbersBlock(block, {
+			singleLine: true
+		});
                 hljs.initLangBlock(block);
         });
 });
