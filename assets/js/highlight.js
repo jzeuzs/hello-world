23aab70b7ea0dbff3290c3fe9c40fadf72da3737
1chi <Nyes3704@gmail.com>
2021-01-10 14:02:03 +0000

    fix again

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
         document.querySelectorAll("pre code").forEach((block) => {
                 hljs.highlightBlock(block);
-                hljs.lineNumbersBlock(block);
+                hljs.lineNumbersBlock(block, { singleLine: true });
                 hljs.initLangBlock(block);
         });
 });
