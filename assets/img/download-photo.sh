4c981bf7e471322102ef0a6cc80a12183e69909d
repl.it user <replituser@example.com>
2021-01-15 10:00:31 +0000

    3rd release

--- assets/img/download-photo.sh
+++ assets/img/download-photo.sh
@@ -1,4 +1,4 @@
 cd assets/img
-wget https://github.com/PKief/vscode-material-icon-theme/raw/master/icons/console.svg
+wget https://github.com/PKief/vscode-material-icon-theme/raw/master/icons/scala.svg
 cd ..
 cd ..
